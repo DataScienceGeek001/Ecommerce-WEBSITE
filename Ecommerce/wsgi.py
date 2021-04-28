6c9dd088c526b117939f0466f65c85d271f63827
DataScienceGeek001 <hpneosft@gmail.com>
2021-04-28 13:15:31 +0530

    Uploading to Heroku

--- Ecommerce/wsgi.py
+++ Ecommerce/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise import WhiteNoise
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce.settings')
 
 application = get_wsgi_application()

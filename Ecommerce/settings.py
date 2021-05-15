dc3167a92b61daa9e3d301d5c92a8c6418678694
DataScienceGeek001 <80503457+DataScienceGeek001@users.noreply.github.com>
2021-05-15 14:11:28 +0530

    Update settings.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -165,8 +165,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "hpneosft@gmail.com"
-EMAIL_HOST_PASSWORD = "Nsft4647"
+EMAIL_HOST_USER = ""  # Enter Your Account name over here
+EMAIL_HOST_PASSWORD = "" # Enter your password here
 
 
 # Static files (CSS, JavaScript, Images)
